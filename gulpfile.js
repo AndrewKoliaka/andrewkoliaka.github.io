3c404883eb981ed446fa80ef77808e3f91385055
AndrewKoliaka <andrew.koliaka@gmail.com>
2017-02-08 16:27:00 +0200

    added build task and dist created

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,47 @@
-var gulp = require('gulp'),
-    browserSync = require('browser-sync').create(),
-    useref = require('gulp-useref'),
-    gulpIf = require('gulp-if'),
-    uglify = require('gulp-uglify'),
-    del = require('del');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const gulpIf = require('gulp-if');
+const uglify = require('gulp-uglify');
+const del = require('del');
+const concat = require('gulp-concat');
+const replace = require('gulp-html-replace');
 
-gulp.task('serve', function() {
+gulp.task('serve', () => {
     browserSync.init({
         server: {
-            baseDir: 'app/'
+            baseDir: 'src/'
         }
     });
 });
 
-gulp.task('watch', function() {
-    gulp.watch('app/src/*.js').on('change', browserSync.reload);
+gulp.task('watch', () => {
+    gulp.watch('src/js/*.js').on('change', browserSync.reload);
 });
 
-gulp.task('build', function() {
-    return gulp.src('app/index.html')
-        .pipe(useref())
+gulp.task('js', () => {
+    gulp.src(['src/js/main.js', 'src/js/grid.js', 'src/js/*.js'])
+        .pipe(concat('app.js'))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('del', function() {
-    del(['dist']);
+gulp.task('css', () => {
+    gulp.src('src/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', () => {
+    gulp.src('src/index.html')
+        .pipe(replace({
+            'css': 'dist/styles.css',
+            'js': 'dist/app.js'
+        }))
+        .pipe(gulp.dest('./'));
+});
+    
+gulp.task('del', () => {
+    del(['dist', 'index.html']);
 });
 
 gulp.task('default', ['serve', 'watch']);
+gulp.task('build', ['js', 'css', 'html']);
